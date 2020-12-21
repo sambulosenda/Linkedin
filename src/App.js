c1bf7aeda4826db3811a51bc1c11b537b9bee718
Sambulo Senda <admin@users-MacBook-Air.local>
2020-12-21 12:40:13 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header.js";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Header */}
+      <Header />
+      {/* App Body */}
+      {/* Feed */}
+      {/* Widget */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./styles.css";
+import SearchIcon from "@material-ui/icons/Search";
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className="header__left">
+        <img src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt="" />
+        <div className="header_search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__right"></div>
+    </div>
+  );
+}
+
+export default Header;
